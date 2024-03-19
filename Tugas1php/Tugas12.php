42b5c271a17352e8156290843a2ed97d62d72d8a
Kireiliz <ocepmorasa@gmail.com>
2024-03-19 22:23:43 +0700

    first commit

---
+++ Tugas1php/Tugas12.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Tugas 1.2</title>
+</head>
+<body>
+    <form>
+        <label for="Kode">Kode</label>
+            <br>
+        <input type="text" id="Kode">
+            <br>
+        <label for="Nama">Nama</label>
+            <br>
+        <input type="text" id="Nama">
+            <br>
+        <label for="Satuan">Satuan</label>
+            <br>
+        <input type="text" id="Satuan">
+            <br> 
+        <label for="Beli">Beli</label>
+            <br>
+        <input type="text" id="Beli">
+            <br>
+        <label for="Jual">Jual</label>
+            <br>
+        <input type="text" id="Jual">
+            <br>   
+            <br>
+        <a href="Tugas1.php" target="_self"><input type="button" name="update" value="Update"></a>
+        <a href="Tugas1.php" target="_self"><input type="button" name="delete" value="Delete"></a>
+        <a href="Tugas1.php" target="_self"><input type="button" name="cancel" value="Cancel"></a>
+    </form>
+</body>
+</html>
