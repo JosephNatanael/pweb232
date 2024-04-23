c0fd822620b6ce423e078b4fc5b7122e9e3129c2
Kireiliz <ocepmorasa@gmail.com>
2024-04-23 09:30:19 +0700

    quiz 1

--- Quiz1/quiz.php
+++ Quiz1/quiz.php
@@ -7,43 +7,6 @@
 
 <h2>Hitung Volume dan Luas Balok, Kubus, Tabung</h2>
 
-<style>
-  input[type="number"],
-  input[type="checkbox"],
-  input[type="button"] {
-    padding: 5px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-  }
-
-  label {
-    display: inline-block;
-    width: 100px;
-    margin-right: 10px;
-    font-weight: bold;
-  }
-
-  button {
-    padding: 10px 20px;
-    border: none;
-    border-radius: 5px;
-    background-color: #007bff;
-    color: #fff;
-    cursor: pointer;
-  }
-
-  button:hover {
-    background-color: #0056b3;
-  }
-  form {
-    background-color: green;
-    padding: 20px;
-    border-radius: 10px;
-    margin-bottom: 20px;
-  }
-</style>
-
-
 <div>
   <input type="checkbox" id="balokCheckbox" onchange="toggleShape('balok')">
   <label for="balokCheckbox">Balok</label>
